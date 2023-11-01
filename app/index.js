00c608efd45fecb8df67ddd94ee2e7e90e477698
ilix-studio <ilishjyoti17@gmail.com>
2023-11-01 12:04:17 +0530

    initial setup

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+
+const index = () => {
+  return (
+    <View>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit obcaecati
+        in distinctio, autem est possimus velit, qui explicabo at adipisci
+        cupiditate harum aliquid perspiciatis neque, earum maiores. Dicta, enim
+        velit!
+      </Text>
+    </View>
+  );
+};
+
+export default index;
---
+++ components/home/nearby/Nearbyjobs.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Nearbyjobs = () => {
+  return (
+    <View>
+      <Text>Nearbyjobs</Text>
+    </View>
+  );
+};
+
+export default Nearbyjobs;
+
+const styles = StyleSheet.create({});
